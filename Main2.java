8dda69fdd09ab16ac13c065f64bb644c88b826cd
sanjit <devkotasanjit@gmail.com>
2024-05-28 22:26:41 +0545

    initial java learn

---
+++ Main2.java
@@ -0,0 +1,15 @@
+ class Main
+{ 
+
+
+    public static void main(String[] args)
+    {
+    System.out.println("Main2 sum");
+    int num1=5;
+    int num2=10;
+    int num3=15;
+    int sum= num1 + num2 + num3;
+    System.out.println(sum);
+}
+}
+
---
+++ sum_of_two_numbers.java
@@ -0,0 +1,14 @@
+  class RamClass
+{ 
+
+
+    public static void main(String[] args)
+    {
+    System.out.println("sum  ram is:");
+    int num1=5;
+    int num2=10;
+    int num3=15;
+    int sum= num1 + num2 + num3;
+    System.out.println(sum);
+}
+}
