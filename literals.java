baa16d55d92897c523e79cab1701293147a7a3ed
Sanjit Devkota <devkotasanjit@gmail.com>
2024-05-29 14:24:24 +0545

    understanding_literals.java

--- literals.java
+++ literals.java
@@ -6,16 +6,17 @@ class literals
     byte weight = 60;
     int age = 35;
     short days = 7;
-    long number = 984500000L;
+    long number = 98********L;
     float marks = 40.6f;
     double salary = 25641.2556d;
     boolean x = true;
     boolean y = false;
-    String str = " NEPAL";
+    String name = " Charles";
     char ch = 'Z';
     System.out.println(days);
     System.out.println(salary);
     System.out.println(marks);
-    System.out.println(weight);
+    System.out.println(name + " " + age + " " + weight);
+                       
 }
 }
