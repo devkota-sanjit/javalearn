df5fae5d53939bb75da13146d81f37569f154874
Sanjit Devkota <devkotasanjit@gmail.com>
2024-05-29 13:12:04 +0545

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,20 @@
+ class literals
+{
+    public static void main (String[] args)
+    {
+    byte weight = 60;
+    int age = 35;
+    short days = 7;
+    long number = 984500000L;
+    float marks = 40.6f;
+    double salary = 25641.2556d;
+    boolean x = true;
+    boolean y = false;
+    String str = " NEPAL";
+    char ch = 'Z';
+    System.out.println(days);
+    System.out.println(salary);
+    System.out.println(marks);
+    System.out.println(weight);
+}
+}
