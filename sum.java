4b6b0dc11cfdf47982dfb2e00fef19b822b4b0c0
Sanjit Devkota <devkotasanjit@gmail.com>
2024-05-29 13:18:46 +0545

    sum.java

--- sum.java
+++ sum.java
@@ -4,7 +4,7 @@
 
     public static void main(String[] args)
     {
-    System.out.println("Main2 sum");
+    System.out.println("Sum of 3 numbers is:");
     int num1=5;
     int num2=10;
     int num3=15;
